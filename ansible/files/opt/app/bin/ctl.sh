e73c64ad369c746a408dabfd3f0ccd33466c7b60
Hongliang Wang <hongliangwang@yunify.com>
2019-06-03 12:44:14 +0800

    Kafka 1.1.1

--- ansible/files/opt/app/bin/ctl.sh
+++ ansible/files/opt/app/bin/ctl.sh
@@ -4,6 +4,11 @@ set -eo pipefail
 
 . /opt/app/bin/.env
 
+command=$1
+log() {
+  logger -t appctl --id=$$ [cmd=$command] $@
+}
+
 svc() {
   systemctl $@ $MY_ROLE
 }
@@ -30,16 +35,15 @@ retry() {
 }
 
 init() {
-  if [ "$MY_ROLE" = "kafka" ]; then
-    mkdir -p /data/kafka/{kafka-logs,logs}
-    chown -R kafka.kafka /data/kafka
-  fi
+  mkdir -p /data/$MY_ROLE/logs
+  chown -R kafka.kafka /data/$MY_ROLE
   svc unmask -q
   svc enable -q
 }
 
 checkPorts() {
-  local port; for port in $MY_PORTS; do nc -z -w3 $MY_IP $port; done
+  local ports="$MY_PORT $EXTRA_PORTS"
+  local port; for port in $ports; do nc -z -w3 $opts $MY_IP $port; done
 }
 
 check() {
@@ -50,6 +54,39 @@ check() {
 start() {
   svc start
   retry 60 1 0 check
+  if [ "$MY_ROLE" = "kafka-manager" ]; then addCluster; fi
+}
+
+addCluster() {
+  curl -s -m5 -w "%{http_code}" -o /dev/null \
+    -u "$WEB_USER:$WEB_PASSWORD" \
+    --data-urlencode "name=$CLUSTER_ID" \
+    --data-urlencode "zkHosts=$ZK_HOSTS" \
+    --data-urlencode "kafkaVersion=$KAFKA_VERSION" \
+    --data-urlencode "jmxUser=" \
+    --data-urlencode "jmxPass=" \
+    --data-urlencode "tuning.brokerViewUpdatePeriodSeconds=30" \
+    --data-urlencode "tuning.clusterManagerThreadPoolSize=2" \
+    --data-urlencode "tuning.clusterManagerThreadPoolQueueSize=100" \
+    --data-urlencode "tuning.kafkaCommandThreadPoolSize=2" \
+    --data-urlencode "tuning.kafkaCommandThreadPoolQueueSize=100" \
+    --data-urlencode "tuning.logkafkaCommandThreadPoolSize=2" \
+    --data-urlencode "tuning.logkafkaCommandThreadPoolQueueSize=100" \
+    --data-urlencode "tuning.logkafkaUpdatePeriodSeconds=30" \
+    --data-urlencode "tuning.partitionOffsetCacheTimeoutSecs=5" \
+    --data-urlencode "tuning.brokerViewThreadPoolSize=2" \
+    --data-urlencode "tuning.brokerViewThreadPoolQueueSize=1000" \
+    --data-urlencode "tuning.offsetCacheThreadPoolSize=2" \
+    --data-urlencode "tuning.offsetCacheThreadPoolQueueSize=1000" \
+    --data-urlencode "tuning.kafkaAdminClientThreadPoolSize=2" \
+    --data-urlencode "tuning.kafkaAdminClientThreadPoolQueueSize=1000" \
+    --data-urlencode "tuning.kafkaManagedOffsetMetadataCheckMillis=30000" \
+    --data-urlencode "tuning.kafkaManagedOffsetGroupCacheSize=1000000" \
+    --data-urlencode "tuning.kafkaManagedOffsetGroupExpireDays=7" \
+    --data-urlencode "securityProtocol=PLAINTEXT" \
+    --data-urlencode "saslMechanism=DEFAULT" \
+    --data-urlencode "jaasConfig=" \
+    "http://$MY_IP:$MY_PORT/clusters"
 }
 
 stop() {
@@ -62,6 +99,29 @@ restart() {
 
 update() {
   if svc is-enabled -q; then restart; fi
+}
+
+measure() {
+  local metrics; metrics=$(echo mntr | nc -u -q3 -w3 127.0.0.1 8125)
+  [ -n "$metrics" ] || return 1
+
+  cat << METRICS_EOF
+  {
+    "heap_usage": $(parseMetrics "$metrics" ".jvm.memory.heap.usage" 100),
+    "MessagesInPerSec_1MinuteRate": $(parseMetrics "$metrics" ".kafka.server.BrokerTopicMetrics.MessagesInPerSec.1MinuteRate"),
+    "BytesInPerSec_1MinuteRate": $(parseMetrics "$metrics" ".kafka.server.BrokerTopicMetrics.BytesInPerSec.1MinuteRate"),
+    "BytesOutPerSec_1MinuteRate": $(parseMetrics "$metrics" ".kafka.server.BrokerTopicMetrics.BytesOutPerSec.1MinuteRate"),
+    "Replica_MaxLag": $(parseMetrics "$metrics" "kafka.server.ReplicaFetcherManager.MaxLag.Replica"),
+    "KafkaController_ActiveControllerCount": $(parseMetrics "$metrics" ".kafka.controller.KafkaController.ActiveControllerCount"),
+    "KafkaController_OfflinePartitionsCount": $(parseMetrics "$metrics" ".kafka.controller.KafkaController.OfflinePartitionsCount")
+  }
+METRICS_EOF
+}
+
+parseMetrics() {
+  local metrics="$1" key="$2" factor
+  [ -z "$3" ] || factor="*$3"
+  echo "$metrics" | xargs -n1 | awk -F: 'BEGIN{value=""} $1=="'$key'"{value=$2} END{print (value=="" ? 0 : value'$factor')}'
 }
 
 $1 ${@:2}
